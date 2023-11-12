1926738592d5928716d1954ab33e1360f0a31029
Victor Hugo Ribeiro Leite da Silva <victorhrls@usp.br>
2023-11-12 09:43:33 -0300

    List books

--- books/views.py
+++ books/views.py
@@ -3,17 +3,18 @@ from django.urls import reverse
 from .temp_data import book_data
 from django.shortcuts import render
 from .models import Book
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 
 
-def detail_book(request, book_id):
-    context = {'book': book_data[book_id - 1]}
-    return render(request, 'books/detail.html', context)
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/detail.html'
 
-
-def list_books(request):
-    context = {"book_list": book_data}
-    return render(request, 'books/index.html', context)
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'books/index.html'
 
 
 def search_books(request):
@@ -30,13 +31,15 @@ def search_books(request):
 
 def create_book(request):
     if request.method == 'POST':
-        book_data.append({
-            'name': request.POST['name'],
-            'release_year': request.POST['release_year'],
-            'capa_url': request.POST['capa_url']
-        })
-        return HttpResponseRedirect(
-            reverse('books:detail', args=(len(book_data), )))
+        book_name = request.POST['name']
+        book_release_year = request.POST['release_year']
+        book_capa_url = request.POST['capa_url']
+        book = Book(name=book_name,
+                      release_year=book_release_year,
+                      capa_url=book_capa_url)
+        book.save()
+        return HttpResponseRedirect(reverse('books:detail',
+                                            args=(book.id, )))
     else:
         return render(request, 'books/create.html', {})
 
